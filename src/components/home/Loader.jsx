dfc8481c9298a13b0bef30d7e06a413602586ed8
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-08-05 20:41:42 +0500

    feat: Loader

---
+++ src/components/home/Loader.jsx
@@ -0,0 +1,11 @@
+function Loader() {
+  return (
+    <>
+      <div className="absolute inset-0 z-50 flex h-full items-center justify-center bg-green-200/20 backdrop-blur-lg">
+        <div className="loader"></div>
+      </div>
+    </>
+  );
+}
+
+export default Loader;
