9dce72e413ba900aaffa966eecac49374fcd641b
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-08-05 20:47:28 +0500

    refactor

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -12,6 +12,9 @@ function ErrorPage() {
       <div className="flex h-screen items-center justify-center">
         <div className="flex flex-col items-center justify-center gap-5">
           <h1 className="text-4xl capitalize">page not found 404</h1>
+          <p className="text-xs">
+            Something went wrong, please try again or press the button.
+          </p>
           <button
             className="rounded-xl border-none bg-green-800 p-5 capitalize shadow-2xl shadow-gray-300/90 outline-none duration-300 hover:bg-green-700 hover:text-black"
             onClick={goHome}
