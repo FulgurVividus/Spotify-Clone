924f6588084da5ca41170ce7f4e11f86390725f3
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-08-06 17:08:46 +0500

    refactor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,19 @@
+import Albums from "./Albums";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <main>
+        <div>
+          <h1>Home</h1>
+        </div>
+
+        <div>
+          <Albums />
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default Home;
