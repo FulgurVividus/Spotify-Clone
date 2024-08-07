6a0762c712f7a2d7d34ad7b797be45559ff07614
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-08-07 18:13:46 +0500

    feat: Search component

---
+++ src/components/search/Search.jsx
@@ -0,0 +1,17 @@
+function Search() {
+  // TODO: Add search functionality
+
+  return (
+    <>
+      <form>
+        <input
+          type="search"
+          placeholder="What do you want to listen to ?"
+          className="w-full max-w-md rounded-3xl border-none p-3 pl-4 text-black outline-none"
+        />
+      </form>
+    </>
+  );
+}
+
+export default Search;
---
+++ src/pages/SearchPage.jsx
@@ -0,0 +1,15 @@
+import Search from "../components/search/Search";
+
+function SearchPage() {
+  // TODO: Add search functionality
+
+  return (
+    <>
+      <div>
+        <Search />
+      </div>
+    </>
+  );
+}
+
+export default SearchPage;
