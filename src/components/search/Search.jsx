0563dc5920bfd4457e81eb077c384202e735f84e
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-08-07 18:37:58 +0500

    feat: search input auto-focus

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,5 +1,13 @@
+import { useEffect, useRef } from "react";
+
 function Search() {
   // TODO: Add search functionality
+
+  const inputEl = useRef(null);
+
+  useEffect(function () {
+    inputEl.current.focus();
+  }, []);
 
   return (
     <>
@@ -8,6 +16,7 @@ function Search() {
           type="search"
           placeholder="What do you want to listen to ?"
           className="w-full max-w-md rounded-3xl border-none p-3 pl-4 text-black outline-none"
+          ref={inputEl}
         />
       </form>
     </>
