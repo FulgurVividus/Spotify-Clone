ccbb086e9e1a4762fcc78ad3f65e8ae9e65e39f4
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-08-08 22:19:48 +0500

    chore

--- src/components/albums/Albums.jsx
+++ src/components/albums/Albums.jsx
@@ -1,31 +1,4 @@
-import { useEffect, useState } from "react";
-import { getAlbums } from "../../services/apiAlbums";
-import Loader from "../home/Loader";
-
 function Albums() {
-  const [albums, setAlbums] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(function () {
-    async function fetchAlbums() {
-      setIsLoading(true);
-
-      try {
-        const data = await getAlbums();
-        setAlbums(data);
-        console.log(data);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    fetchAlbums();
-  }, []);
-
-  if (isLoading) return <Loader />;
-
   return (
     <>
       <div>
