dc5a24bd9e6e514ddbf7f1da0b8e825fb719d3f9
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-08-10 15:51:11 +0500

    styles

--- src/components/home/CategoriesItem.jsx
+++ src/components/home/CategoriesItem.jsx
@@ -1,7 +1,7 @@
 function CategoriesItem({ category }) {
   return (
     <>
-      <div className="group flex flex-col gap-1">
+      <div className="group flex flex-col items-center gap-1">
         <h2 className="text-md max-w-fit cursor-default rounded-lg bg-white p-1 text-black duration-300 group-hover:bg-green-600">
           {category.name}
         </h2>
