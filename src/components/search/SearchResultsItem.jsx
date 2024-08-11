c0998573663bc006c4c95476480ec0e256fd4bb7
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-08-11 21:21:18 +0500

    refactor & styles

--- src/components/search/SearchResultsItem.jsx
+++ src/components/search/SearchResultsItem.jsx
@@ -1,28 +1,28 @@
 import { useNavigate } from "react-router-dom";
 
-function SearchResultsItem({ album }) {
+function SearchResultsItem({ artistTrack }) {
   const navigate = useNavigate();
 
   function handleNavigate() {
-    navigate(`/search/${album.id}`, { state: { album } });
+    navigate(`/search/${artistTrack.id}`, { state: { artistTrack } });
   }
 
   return (
     <>
       <div
         onClick={handleNavigate}
-        className="h-100 flex max-w-xs cursor-pointer flex-col rounded-xl bg-neutral-600 px-3 py-4 duration-300 hover:bg-green-700 hover:opacity-75"
+        className="flex max-w-full cursor-pointer rounded-xl bg-neutral-600 px-3 py-2 duration-300 hover:bg-green-700 hover:opacity-75"
       >
         <div className="flex-grow overflow-hidden rounded-full">
           <img
-            src={album.images.at(1).url}
-            alt={`Image of ${album.name}`}
+            src={artistTrack.album.images.at(2).url}
+            alt={`Image of ${artistTrack.name}`}
             className="h-100 w-full max-w-fit rounded-full bg-center object-cover duration-300 hover:scale-110"
           />
         </div>
 
         <h2 className="text-md mt-2 text-left font-semibold text-white">
-          {album.name}
+          {artistTrack.name}
         </h2>
       </div>
     </>
