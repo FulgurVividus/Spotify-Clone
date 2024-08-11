9f19a4474f92edb6e2a6fb83b9f45bba83f838e2
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-08-11 14:38:33 +0500

    styles

--- src/components/search/SearchResultsItem.jsx
+++ src/components/search/SearchResultsItem.jsx
@@ -13,11 +13,11 @@ function SearchResultsItem({ album }) {
         onClick={handleNavigate}
         className="h-100 flex max-w-xs cursor-pointer flex-col rounded-xl bg-neutral-600 px-3 py-4 duration-300 hover:bg-green-700 hover:opacity-75"
       >
-        <div className="flex-grow overflow-hidden rounded-xl">
+        <div className="flex-grow overflow-hidden rounded-full">
           <img
             src={album.images.at(1).url}
             alt={`Image of ${album.name}`}
-            className="h-100 w-full max-w-fit rounded-xl object-cover duration-300 hover:scale-110"
+            className="h-100 w-full max-w-fit rounded-full bg-center object-cover duration-300 hover:scale-110"
           />
         </div>
 
